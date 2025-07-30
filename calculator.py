d16012c7f25034e598daaad8b971bf9296e02e2c
learntech22 <mich22engineer@gmail.com>
2025-07-30 11:00:10 +0200

    Added greeting message and logic seperated from main file, into seperate function

--- calculator.py
+++ calculator.py
@@ -1,18 +1,4 @@
-from operations import add, subtract, multiply, divide
+from operations import calculate
 
-num1 = float(input("Enter first number: "))
-operator = input("Enter operator (+, -, *, /): ")
-num2 = float(input("Enter second number: "))
-
-if operator == '+':
-    result = add(num1, num2)
-elif operator == '-':
-    result = subtract(num1, num2)
-elif operator == '*':
-    result = multiply(num1, num2)
-elif operator == '/':
-    result = divide(num1, num2)
-else:
-    result = "Invalid operator"
-
-print("Result:", result)
+print("Hi, Let's use basic calculator")
+calculate()
--- operations.py
+++ operations.py
@@ -11,3 +11,24 @@ def divide(a, b):
     if b == 0:
         return "Error: Cannot divide by zero"
     return a / b
+
+def calculate():
+    num1 = float(input("Enter first number: "))
+    
+    operator = input("Enter operator (+, -, *, /): ")
+    while operator not in ['+', '-', '*', '/']:
+        print("Invalid operator! Please enter one of +, -, *, /")
+        operator = input("Enter operator (+, -, *, /): ")
+    
+    num2 = float(input("Enter second number: "))
+    
+    if operator == '+':
+        result = add(num1, num2)
+    elif operator == '-':
+        result = subtract(num1, num2)
+    elif operator == '*':
+        result = multiply(num1, num2)
+    elif operator == '/':
+        result = divide(num1, num2)
+    
+    print("Result:", result)
